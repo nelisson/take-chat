e21374beaf9137ba2b3b07e400dd8b9270e37704
Nelisson Cavalheiro <nelisson.ad@gmail.com>
2021-08-20 14:33:40 -0300

    feat: added User entity and tests

---
+++ TakeChat.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TakeChat.Domain.Entities
+{
+    public class User
+    {
+        public string Name { get; }
+
+        public User(string name)
+        {
+            if (string.IsNullOrEmpty(name.Trim()))
+            {
+                throw new ArgumentException("Nome de usuário não pode ser deixado em branco");
+            }
+
+            if (name.Trim().Length > 50)
+            {
+                throw new ArgumentOutOfRangeException(nameof(name), "Nome de usuário não pode conter mais que 50 caracteres");
+            }
+
+            Name = name;
+        }
+    }
+}
