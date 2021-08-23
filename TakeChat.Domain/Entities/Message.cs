e38cc184bf55fe6f34758aa018bed142fb4fc63c
Nelisson Cavalheiro <nelisson.ad@gmail.com>
2021-08-22 21:34:08 -0300

    refactor: changing exception messages language to english

--- TakeChat.Domain/Entities/Message.cs
+++ TakeChat.Domain/Entities/Message.cs
@@ -14,22 +14,22 @@ namespace TakeChat.Domain.Entities
         {
             if (string.IsNullOrEmpty(from.Trim()))
             {
-                throw new ArgumentException("Campo remetente não pode ser deixado em branco");
+                throw new ArgumentException("From field cannot be empty", nameof(from));
             }
 
             if (string.IsNullOrEmpty(to.Trim()) && string.IsNullOrEmpty(channel.Trim()))
             {
-                throw new ArgumentException("Pelo menos um campos precisa estar preenchido (destinatário ou sala)");
+                throw new ArgumentException("Almost one field needs to be filled (To or Channel)");
             }
 
             if (string.IsNullOrEmpty(body.Trim()))
             {
-                throw new ArgumentException("Corpo da mensagem não pode ser deixado em branco");
+                throw new ArgumentException("Message Body cannot be empty", nameof(body));
             }
 
             if (body.Trim().Length > 144)
             {
-                throw new ArgumentOutOfRangeException(nameof(body), "Corpo da mensagem não pode conter mais que 144 caracteres");
+                throw new ArgumentOutOfRangeException(nameof(body), "Message Body cannot contain more than 144 characters.");
             }
 
             From = from;
