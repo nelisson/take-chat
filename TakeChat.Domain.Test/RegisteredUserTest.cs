80c2d6532095b179097e08676be0712edfae6b27
Nelisson Cavalheiro <nelisson.ad@gmail.com>
2021-08-22 22:03:49 -0300

    feat: added RegisteredUser and tests

---
+++ TakeChat.Domain.Test/RegisteredUserTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TakeChat.Domain.Entities;
+using Xunit;
+
+namespace TakeChat.Domain.Test
+{
+    public class RegisteredUserTest
+    {
+        private const string GENERAL_CHANNEL = "general";
+        private const string A_MESSAGE = "The quick brown fox jumps over the lazy dog";
+
+        [Fact]
+        public void Constructor_ValidParameters_ReturnsOK()
+        {
+            using var memoryStream = new MemoryStream();
+            var username = Faker.Internet.UserName();
+            var channel = GENERAL_CHANNEL;
+
+            var registeredUser = new RegisteredUser(memoryStream)
+            {
+                Username = username,
+                Channel = channel
+            };
+
+
+            Assert.Equal(username, registeredUser.Username);
+            Assert.Equal(channel, registeredUser.Channel);
+        }
+        
+        [Fact]
+        public void SendMessage_WritingToStream_AreCorrectlySended()
+        {
+            using var memoryStream = new MemoryStream();
+            
+            var usernameFrom = Faker.Internet.UserName();
+            var usernameTo = Faker.Internet.UserName();
+            var channel = GENERAL_CHANNEL;
+            var body = A_MESSAGE;
+
+            var message = new Message(usernameFrom, usernameTo, channel, body);
+            var jsonMessage = JsonSerializer.Serialize(message);
+
+            var registeredUser = new RegisteredUser(memoryStream);
+
+            registeredUser.SendMessage(message);
+
+            var readedData = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            Assert.Equal(jsonMessage, readedData);
+        }
+
+        [Fact]
+        public void WaitForMessage_ReadFromStream_AreCorrectlyReceived()
+        {
+            var usernameFrom = Faker.Internet.UserName();
+            var usernameTo = Faker.Internet.UserName();
+            var channel = GENERAL_CHANNEL;
+            var body = A_MESSAGE;
+            
+            var message = new Message(usernameFrom, usernameTo, channel, body);
+            var jsonMessage = JsonSerializer.Serialize(message);
+            
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+            var registeredUser = new RegisteredUser(memoryStream);            
+            var receivedMessage = registeredUser.WaitForMessage();
+
+            Assert.Equal(message, receivedMessage);
+        }
+    }
+}
---
+++ TakeChat.Domain/Entities/RegisteredUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using TakeChat.Domain.Interfaces;
+
+namespace TakeChat.Domain.Entities
+{
+    public class RegisteredUser : IRegisteredUser
+    {
+        public string Username { get; set; }
+        public string Channel { get; set; }
+        private const int BUFFER_SIZE = 1024;
+        private readonly Stream _stream;
+
+
+        public RegisteredUser(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public void SendMessage(Message message)
+        {
+            var data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+            _stream.Write(data, 0, data.Length);
+        }
+
+        public Message WaitForMessage()
+        {
+            var buffer = new byte[BUFFER_SIZE];
+            int position = _stream.Read(buffer, 0, buffer.Length);
+            var jsonResponse = Encoding.UTF8.GetString(buffer, 0, position);
+
+            return JsonSerializer.Deserialize<Message>(jsonResponse);
+        }
+    }
+}
