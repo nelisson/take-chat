129b3db0b331fa5e5903ff9b2ea6e960210856e6
Nelisson Cavalheiro <nelisson.ad@gmail.com>
2021-08-22 22:05:35 -0300

    refactor: extract interface from RegisteredUser

---
+++ TakeChat.Domain/Interfaces/IRegisteredUser.cs
@@ -0,0 +1,12 @@
+﻿using TakeChat.Domain.Entities;
+
+namespace TakeChat.Domain.Interfaces
+{
+    public interface IRegisteredUser
+    {
+        string Channel { get; set; }
+        string Username { get; set; }
+        void SendMessage(Message message);
+        Message WaitForMessage();
+    }
+}
